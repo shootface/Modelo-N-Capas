c285da12d922ea817d1f4cdfb311ab9e28cc6d29
Diego Fernando Guiza Galeano <diego.guizag@campusuuc.edu.co>
2020-08-31 21:36:39 -0500

    Capa datos

--- Modelo n Capas/src/capaDatos/DatosUsuarios.java
+++ Modelo n Capas/src/capaDatos/DatosUsuarios.java
@@ -1,16 +1,15 @@
-package com.fa11en.json.dataaccesslayer;
 
 import com.fa11en.json.entitylayer.User;
 import org.json.simple.JSONArray;
 
-public class Datos_Usuarios {
+public class DatosUsuarios {
 
     public JSONArray arrayUsuarios(){
         JSONArray arr = new JSONArray();
             arr.add(new User(10, "Diego Fernando Güiza", "1234"));
             arr.add(new User(11, "Juan Camilo Guaba", "JCGD"));
             arr.add(new User(12, "Wilmer Ricardo Pachon", "WRPD"));
-        return arr;
+        return arr;  
     }
 
 }
