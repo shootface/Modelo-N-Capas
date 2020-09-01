75643920d9b32e1b8006caeefa4f8904c25a6869
shootface <wava0324@gmail.com>
2020-08-31 21:34:09 -0500

    begin bussines layer

---
+++ Modelo n Capas/src/capaNegocio/Negocio.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package capaNegocio;
+
+import capaEntidad.Usuario;
+import org.json.simple.JSONArray;
+
+
+public class Negocio {
+    
+    public JSONArray arregloUsuario(){
+        Datos_Usuarios du = new Datos_Usuarios();
+        
+    }
+}
